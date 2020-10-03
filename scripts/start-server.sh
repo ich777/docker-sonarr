f08e5e52a1f54406425cbbc4c0c3c78fc53190a1
ich777 <ich777@noreply.example.org>
2020-10-03 23:56:09 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sleep infinity
 if [ "$SONARR_REL" == "latest" ]; then
-    LAT_V="$(wget -qO- https://git.minenet.at/ich777/versions/raw/branch/master/Sonarr | grep LATEST | cut -d '=' -f2)"
+    LAT_V="$(wget -qO- https://github.com/ich777/versions/raw/master/Sonarr | grep LATEST | cut -d '=' -f2)"
 elif [ "$SONARR_REL" == "nightly" ]; then
-    LAT_V="$(wget -qO- https://git.minenet.at/ich777/versions/raw/branch/master/Sonarr | grep NIGHTLY | cut -d '=' -f2)"
+    LAT_V="$(wget -qO- https://github.com/ich777/versions/raw/master/Sonarr | grep NIGHTLY | cut -d '=' -f2)"
 else
     echo "---Version manually set to: v$SONARR_REL---"
     LAT_V="$SONARR_REL"
